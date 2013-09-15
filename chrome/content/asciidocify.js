a58339d61dc5707c221de58d551bb0ec2bae561c
Guillaume Grossetie <g.grossetie@gmail.com>
2013-09-15 13:25:54 +0200

    Version 0.1

---
+++ chrome/content/asciidocify.js
@@ -0,0 +1,107 @@
+window.addEventListener('load', function load() {
+    window.removeEventListener('load', load, false);
+    asciidoctorpreview.load();
+}, false);
+
+if (typeof asciidoctorpreview === 'undefined') {
+
+    var ASCIIDOCTOR_OPTIONS = Opal.hash2([ 'attributes' ], {
+        'attributes':[ 'notitle!' ]
+    });
+
+    var asciidoctorpreview = {
+
+        load:function () {
+            var appcontent = document.getElementById('appcontent');
+            if (appcontent) {
+                appcontent.addEventListener('DOMContentLoaded', this.onPageLoad, true);
+            }
+            var prefs = getAddonPrefs();
+            var enabled = isAddonEnabled(prefs);
+            updateAddonIcon(enabled);
+        },
+
+        onPageLoad:function (aEvent) {
+            var document = aEvent.originalTarget;
+            var regexpAdFile = /\.a(sciidoc|doc|d)$/i;
+            if (regexpAdFile.test(document.location)) {
+                if (isAddonEnabled()) {
+                    appendStyles(document);
+                    render(document);
+                }
+            }
+        },
+
+        onMenuItemCommand:function (e) {
+            var prefs = getAddonPrefs();
+            var enabled = isAddonEnabled(prefs);
+
+            // Switch enabled <> disabled
+            enabled = !enabled;
+
+            // Update the extension icon
+            updateAddonIcon(enabled);
+
+            // Save the new status of the extension
+            prefs.setBoolPref("enabled", enabled);
+
+            // Reload the page
+            reloadPage();
+        },
+
+        onToolbarButtonCommand:function (e) {
+            asciidoctorpreview.onMenuItemCommand(e);
+        }
+    };
+
+    function updateAddonIcon(enabled) {
+        // Update the extension icon
+        var iconName = enabled ? "enabled.png" : "disabled.png";
+        document.getElementById('asciidoctorpreview-toolbar-button').style.listStyleImage = 'url("chrome://asciidoctorpreview/skin/' + iconName + '")';
+    }
+
+    function reloadPage() {
+        var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+            .getService(Components.interfaces.nsIWindowMediator);
+        var mainWindow = wm.getMostRecentWindow("navigator:browser");
+        mainWindow.gBrowser.selectedTab.linkedBrowser.reload();
+    }
+
+    function getAddonPrefs() {
+        var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+            .getService(Components.interfaces.nsIPrefService)
+            .getBranch("extensions.asciidoctorpreview.");
+        prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
+        return prefs;
+    }
+
+    function isAddonEnabled(prefs) {
+        if (typeof prefs === 'undefined') {
+            prefs = getAddonPrefs();
+        }
+        var enabled = prefs.getBoolPref("enabled");
+        return enabled;
+    }
+
+    /**
+     * Render AsciiDoc content as HTML
+     */
+    function render(document) {
+        var content = document.firstChild;
+        var data = content.textContent;
+        document.body.innerHTML = '';
+        var generatedHtml = Opal.Asciidoctor.$render(data, ASCIIDOCTOR_OPTIONS);
+        document.body.innerHTML = "<div id='content'>" + generatedHtml + "</div>";
+    }
+
+    /**
+     * Append css files
+     */
+    function appendStyles(document) {
+        var asciidoctorLink = document.createElement('link');
+        asciidoctorLink.rel = 'stylesheet';
+        asciidoctorLink.id = 'asciidoctor-style';
+        asciidoctorLink.href = "resource://asciidoctorpreview/asciidoctor.css";
+        document.head.appendChild(asciidoctorLink);
+    }
+}
---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,9 @@
+// Default preference values. These are accessible via the preferences system
+// or via the optional chrome/content/options.xul preferences dialog.
+
+// These are here as an example only. Please remove them or rename them to
+// something useful.
+pref("extensions.asciidoctorpreview.enabled", true);
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.asciidoctor-firefox-addon@asciidoctor.org.description", "chrome://asciidoctorpreview/locale/overlay.properties");
